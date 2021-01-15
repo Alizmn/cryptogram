1f407ae922652ee4cc7ceca012ed5efd8e412fe9
Ali Zamani <Ali.zmn@hotmail.com>
2021-01-15 13:46:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+export default function App() {
+  const convertedKey = process.env.REACT_APP_NOMIC_KEY;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
-}
-
-export default App;
+};
